857bdd49ec6d515e0a13f2d2bdacbde9134f7d5c
ashwinm2 <ashwinspocket@gmail.com>
2016-04-11 13:45:19 -0700

    Added comments to it.

--- Trail/JavaApplication2/src/javaapplication2/Trial.java
+++ Trail/JavaApplication2/src/javaapplication2/Trial.java
@@ -15,7 +15,10 @@ class Trial
 {
     HashMap<Integer, List<Integer>> hmap = new HashMap<>();
     
-    
+    /* Function that sets the position of the portals for the 
+    * duration of the game.
+    * @param none
+    */
     void init()
     {
         for( int i = 0;i < 8;i++)
@@ -25,6 +28,10 @@ class Trial
   
     }
     
+    /* Function that displays the information regarding all the portals. 
+    *  Used by developers for verification.
+    * @param none
+    */
     void print()
     {
         for(int key : hmap.keySet())
@@ -33,6 +40,13 @@ class Trial
         }
     }
     
+    
+    /* Function that computes the position of each portal in the game board and their c
+    *  corresponding attributes. Their attributes correspod to information such as 
+    *  whether the portal will push down(-1) or up(+1), how many squares will the pawn 
+    *  be moved and final square in which the pawn will land.
+    * @param none
+    */
     void set_portals()
     {
         Random rand = new Random();
@@ -41,29 +55,28 @@ class Trial
         int moving_spaces = 0;
         int final_pos = 0;
         
-        int flag = 0;
-        
         portal_pos = rand.nextInt((99 - 2) + 1) + 2;
  
-        while(hmap.containsKey(portal_pos))
+        while(hmap.containsKey(portal_pos))         // If the position already exist in the map.
         {
             portal_pos = rand.nextInt((99 - 2) + 1) + 2;
         }
             
         if(portal_pos < 10 && portal_pos > 2 )
             orientation = 1;
-        else if(portal_pos >= 99 || portal_pos <= 90) 
+        else if(portal_pos <= 99 && portal_pos >= 90) 
+            orientation = -1;
+        else
         {
             int temp = rand.nextInt((1 - 0) + 1) + 0;
             if(temp == 0)
                 orientation = 1;
             else
                 orientation = -1;
-        } else
-            orientation = -1;
+        }
             
         int check = 0;
-        while(check == 0)
+        while(check == 0)                           // Flag to check whether the resulting square is within range of the Game board.
         {
             int moving = rand.nextInt((30 - 5) + 1) + 5;
             int sum;
@@ -71,6 +84,7 @@ class Trial
                 sum = portal_pos + moving;
             else
                 sum = portal_pos - moving;
+            
             if(sum < 99 && sum > 2)
             {
                 moving_spaces = moving;
@@ -78,12 +92,35 @@ class Trial
                 check = 1;
             }
         }
-        
         List<Integer> myList = new ArrayList<>();
         myList.add(orientation);
         myList.add(moving_spaces);
         myList.add(final_pos);
         hmap.put(portal_pos,myList);
         
+    }
+    
+    
+    /* Function that checks whether the passed square number has a portal 
+    *  associated with it. If it is true then the function returns the 
+    *  square number to which the pawn needs to be moved, else it returns -1.
+    *  
+    * @param position of the square.
+    */
+    int check_portal(int pos)
+    {
+        if(hmap.containsKey(pos))
+        {
+            List<Integer> temp;
+            for(int key : hmap.keySet())
+            {
+                if(key == pos)
+                {
+                    temp = hmap.get(pos);
+                    return temp.get(2);             // Returns the position to which the pwan needs to be moved.
+                }
+            }
+        }
+        return -1;                                  //  If the square does not have a portal associatd with it.
     }
 }
