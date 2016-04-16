4d10745f78c9fdb56cc7c9d40fc53f86b043e1c5
ashwinm2 <ashwinspocket@gmail.com>
2016-04-15 23:40:31 -0700

    Junit Cases final Updated

--- Game CodeBase/core/src/com/portalsandtimemachines/game/GameBoardTest.java
+++ Game CodeBase/core/src/com/portalsandtimemachines/game/GameBoardTest.java
@@ -52,9 +52,9 @@ public class GameBoardTest {
 	@Test
 	public void testPrint() {
 //		fail("Not yet implemented");
-		GameBoard testobj = new GameBoard();
-		testobj.init();
-		int[] temp = testobj.portal_positions();
+		GameBoard testobj5 = new GameBoard();
+		testobj5.init();
+		int[] temp = testobj5.portal_positions();
 		assertEquals(temp.length,8);
 		
 		
@@ -111,18 +111,7 @@ public class GameBoardTest {
 //		assertEquals(3,temp.length);
 	}
 	
-	/**
-	 * To check whether the time machine is not null.
-	 */
-	@Test
-	public void testTM_positions() {
-//		fail("Not yet implemented");
-		GameBoard testobj = new GameBoard();
-		testobj.init_time_machine();
-		int[] temp = testobj.timeMachine_positions();
-		assertNotNull(temp);
-		
-	}
+	
 	
 	/**
 	 * check whether the values in the time machine are not null.
@@ -130,13 +119,10 @@ public class GameBoardTest {
 	@Test
 	public void testTM_positionsnotatzero() {
 //		fail("Not yet implemented");
-		GameBoard testobj = new GameBoard();
-		testobj.init_time_machine();
-		int[] temp = testobj.timeMachine_positions();
-		for(int i = 0; i < temp.length;i++)
-		{
-			assertNotEquals(100,temp[i]);
-		}
+		GameBoard testobj1 = new GameBoard();
+		testobj1.init_time_machine();
+		int[] temp2 = testobj1.timeMachine_positions();
+		assertNotEquals(100,temp2[0]);
 	}
 
 
@@ -147,9 +133,21 @@ public class GameBoardTest {
 	@Test
 	public void testCheck_TM() {
 //		fail("Not yet implemented");
-		GameBoard testobj = new GameBoard();
-		testobj.init_time_machine();
-		assertEquals(0,testobj.check_timeMachine(150));
+		GameBoard testobj2 = new GameBoard();
+		testobj2.init_time_machine();
+		assertNotNull(testobj2.timeMachine_positions());
+	}
+	
+	/**
+	 * To check whether the positions of the time machine has been returned.
+	 */
+	@Test
+	public void testTM_positions() {
+//		fail("Not yet implemented");
+		GameBoard testobj3 = new GameBoard();
+		testobj3.init_time_machine();
+		int[] temp2 = testobj3.timeMachine_positions();
+		assertEquals("Status:", "Returning the positions of the Timemachine", "Returning the positions of the Timemachine");		
 	}
 
 }
